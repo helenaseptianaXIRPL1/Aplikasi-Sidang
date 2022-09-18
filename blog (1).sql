3d5be68e81dd513076029518da4e163bcb343601
Makhluk Goa <88118215+helenaseptianaXIRPL1@users.noreply.github.com>
2022-09-18 12:18:49 +0700

    Add files via upload

---
+++ blog (1).sql
@@ -0,0 +1,237 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 18, 2022 at 07:18 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.3.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barangs`
+--
+
+CREATE TABLE `barangs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama_barang` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `harga` double NOT NULL,
+  `stok` int(11) NOT NULL,
+  `keterangan` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `barangs`
+--
+
+INSERT INTO `barangs` (`id`, `nama_barang`, `harga`, `stok`, `keterangan`, `created_at`, `updated_at`) VALUES
+(1, 'L\'amara', 2000000000, 494, 'L\'amara termasuk Clean & Natural Beauty, formulasinya memadukan bahan-bahan alami yang berkualitas tinggi, dan bebas dari bahan pengisi dan bahan kimia berbahaya bagi kesehatan dan kecantikan. Yang terdiri dari :', NULL, '2022-09-12 18:59:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(21, '2014_10_12_000000_create_users_table', 1),
+(22, '2014_10_12_100000_create_password_resets_table', 1),
+(23, '2022_08_23_011251_create_barangs_table', 1),
+(24, '2022_08_23_012950_create_pesanans_table', 1),
+(25, '2022_08_23_013334_create_pesanan_details_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanans`
+--
+
+CREATE TABLE `pesanans` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kode` int(11) NOT NULL,
+  `jumlah_harga` bigint(20) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pesanans`
+--
+
+INSERT INTO `pesanans` (`id`, `user_id`, `tanggal`, `status`, `kode`, `jumlah_harga`, `created_at`, `updated_at`) VALUES
+(1, 1, '2022-09-06', '1', 388, 2000000000, '2022-09-05 21:22:26', '2022-09-05 21:22:46'),
+(2, 1, '2022-09-11', '1', 484, 6000000000, '2022-09-11 01:36:28', '2022-09-11 01:36:37'),
+(3, 1, '2022-09-11', '1', 231, 4000000000, '2022-09-11 01:45:24', '2022-09-12 18:59:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pesanan_details`
+--
+
+CREATE TABLE `pesanan_details` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `barang_id` int(11) NOT NULL,
+  `pesanan_id` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `jumlah_harga` bigint(20) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pesanan_details`
+--
+
+INSERT INTO `pesanan_details` (`id`, `barang_id`, `pesanan_id`, `jumlah`, `jumlah_harga`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 1, 2000000000, '2022-09-05 21:22:26', '2022-09-05 21:22:26'),
+(2, 1, 2, 3, 6000000000, '2022-09-11 01:36:28', '2022-09-11 01:36:28'),
+(4, 1, 3, 2, 4000000000, '2022-09-12 18:59:44', '2022-09-12 18:59:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `no_hp` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `alamat`, `no_hp`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'helenayahuu', 'pembencikoruptor17@gmail.com', NULL, '$2y$10$2oZQYgmD5Uj.07CS1NeIgO.f/o0Mt9PDoLLwEiBvvHKUwaBDwONqC', 'kp.lebaksari rt.04/06', '0867568956', NULL, '2022-09-05 02:41:55', '2022-09-05 21:37:13');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barangs`
+--
+ALTER TABLE `barangs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `pesanans`
+--
+ALTER TABLE `pesanans`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pesanan_details`
+--
+ALTER TABLE `pesanan_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barangs`
+--
+ALTER TABLE `barangs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- AUTO_INCREMENT for table `pesanans`
+--
+ALTER TABLE `pesanans`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `pesanan_details`
+--
+ALTER TABLE `pesanan_details`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
